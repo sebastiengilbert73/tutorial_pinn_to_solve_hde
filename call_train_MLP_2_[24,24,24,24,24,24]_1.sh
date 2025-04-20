c91b2f08f572561500bdb501a3849f08981a6ee1
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2025-04-20 09:43:53 -0400

    '.\call_train_MLP_2_[24,24,24,24,24,24]_1.sh' .\call_train_ResidualNet_2_6_24_1.sh .\call_train_Wang2020_2_6_24_1.sh: 1st commit

---
+++ call_train_MLP_2_[24,24,24,24,24,24]_1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ./training
+python ./train.py \
+	--outputDirectory="./output_train" \
+	--randomSeed=0 \
+	--initialProfile="./heated_segments.csv" \
+	--architecture="MLP_2_[24,24,24,24,24,24]_1" \
+	--duration=10.0 \
+	--alpha=0.0001 \
+	--scheduleFilepath="./schedule.csv" \
+	--numberOfBoundaryPoints=256 \
+	--numberOfDiffEquResPoints=256 \
+	--displayResults
+	
